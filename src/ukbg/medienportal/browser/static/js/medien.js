c8b6db9a4173bd7ae9b106d470c514aed766cb82
trabby <info@trabucchi.de>
2018-02-21 14:17:36 +0100

    turning off the Js alert

--- src/ukbg/medienportal/browser/static/js/medien.js
+++ src/ukbg/medienportal/browser/static/js/medien.js
@@ -1,5 +1,5 @@
-(function($) {
- $(document).ready(function(){
-     alert("Custom JS Active!");
- });
-})(jQuery);
+//**(function($) {
+ //$(document).ready(function(){
+   //  alert("Custom JS Active!");
+ //});
+//})(jQuery);**//
